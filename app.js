d252559697a8f628698fe022cd6ea52fe2989f61
avi <contact.avi22@gmail.com>
2023-09-25 17:47:54 +0530

    Initial commit

---
+++ app.js
@@ -0,0 +1,17 @@
+import express from "express";
+import { dirname } from "path";
+import { fileURLToPath } from "url";
+
+const app = express();
+const port = 3000;
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
+app.use(express.static(__dirname + "/public"));
+
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/public/index.html");
+});
+
+app.listen(port, () => {
+  console.log(`Website listening on ${port}`);
+});
